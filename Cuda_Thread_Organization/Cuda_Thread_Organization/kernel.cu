9251bd6e49d39e404a3c658c25b4697f1ea4f63d
Min shuo Huang <asdzxc135903164@gmail.com>
2024-02-24 14:30:59 +0800

    add thread organization 0224

---
+++ Cuda_Thread_Organization/Cuda_Thread_Organization/kernel.cu
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+__global__ void helloWorld()
+{
+	const int a = blockIdx.x;
+	const int b = blockIdx.y;
+	const int c = blockIdx.z;
+	const int tx = threadIdx.x;
+	const int ty = threadIdx.y;
+	const int tz = threadIdx.z;
+	int tid = threadIdx.z * blockDim.x * blockDim.y + threadIdx.y * blockDim.x + threadIdx.x;
+	int bid = blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
+	int total_tid = bid * blockDim.x * blockDim.y * blockDim.z + tid;
+	printf("Hello World from block-(%d, %d, %d)and thread-(%d, %d, %d)!--%d\n", a, b, c, tx, ty, tz, total_tid);
+}
+int main()
+{
+	const dim3 block_size(1, 2, 3);
+	const dim3 grid_size(2, 4, 2);
+	helloWorld << <grid_size, block_size >> > ();
+	cudaDeviceSynchronize();
+}
