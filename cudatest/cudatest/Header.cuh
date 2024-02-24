cf090b8891de7ac51c8e5a045b40ea9d36904029
Min shuo Huang <asdzxc135903164@gmail.com>
2024-02-24 10:29:20 +0800

    0224push

---
+++ cudatest/cudatest/Header.cuh
@@ -0,0 +1,21 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+__global__ void myfirstkernel(void)
+{
+	printf("Hello!!!I'm thread: %d in block: %d\n", threadIdx.x, blockIdx.x);
+}
+class GPU_class
+{
+private:
+	int block, thread;
+public:
+	GPU_class(int block, int thread) :block(block), thread(thread)
+	{
+		printf("establish gpu class object with %d thread, %d block\n", this->thread, this->block);
+	}
+	void calldevice(void)
+	{
+		myfirstkernel << <this->block, this->thread >> > ();
+	}
+};
---
+++ cudatest/cudatest/kernel.cu
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "Header.cuh"
+
+int main(void) {
+	GPU_class gpu(1, 1024);
+	gpu.calldevice();
+	cudaDeviceSynchronize();
+	printf("All threads are finished!\n");
+	system("Pause");
+	return 0;
+}
+z
